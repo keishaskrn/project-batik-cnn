362ddea9b4cf0207ed17d241f9d11f07ac66b4b6
kk <rakakagem@gmail.com>
2026-06-16 09:33:53 +0700

    Configure Railway deployment

--- app.py
+++ app.py
@@ -200,4 +200,11 @@ def api_predict():
 # ============================================================
 if __name__ == '__main__':
     load_model_and_labels()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
