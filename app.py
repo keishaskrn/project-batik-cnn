29b88eba6c30f8d110c1d9ad12294386f88e4a22
kk <rakakagem@gmail.com>
2026-06-16 09:56:58 +0700

    Render deploy fix

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        host="0.0.0.0",
+        host='0.0.0.0',
         port=port,
         debug=False
     )
